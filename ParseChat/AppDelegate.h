fb460e9dd69564eba43ac763fc2c0dca7694e259
Adrienne Li <ahli@fb.com>
2020-07-06 11:16:05 -0500

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by Adrienne Li on 7/6/20.
+//  Copyright © 2020 ahli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
